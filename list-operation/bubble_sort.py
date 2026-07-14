42876e67321d2a6ee95db20a27349d1dda470b3e
andrei_manescu <157368799+andreim10@users.noreply.github.com>
2026-07-14 15:40:00 +0300

    Update bubble_sort.py

--- list-operation/bubble_sort.py
+++ list-operation/bubble_sort.py
@@ -12,13 +12,13 @@ def bubble_sort(numbers):
 
  #Read the numbers to be sorted from the user.
 def get_numbers_from_user():
-    count = int(input("Câte numere vrei să sortezi? "))
-    numbers = [float(input("Numărul {i + 1}: ")) for i in range(count)]
+    count = int(input("How many numbers do you want to be sorted?"))
+    numbers = [float(input("Number {i + 1}: ")) for i in range(count)]
     return numbers
 
 #The main part of the program, where the functions defined above are executed.
 if __name__ == "__main__":
     data = get_numbers_from_user()
-    print("Lista introdusă:", data)
+    print("The entered list:", data)
     sorted_data = bubble_sort(data)
-    print("Lista sortată:", sorted_data)
+    print("Sorted list:", sorted_data)
